9fa473d3d67a6ae4365d77555f403793de05230b
TabooSun <taboosun1996@gmail.com>
2021-09-17 21:16:14 +0800

    Fix unable to compile on Android

--- android/src/main/kotlin/com/tech_creator/dr_libphonenumber/dr_libphonenumber/DrLibphonenumberPlugin.kt
+++ android/src/main/kotlin/com/tech_creator/dr_libphonenumber/dr_libphonenumber/DrLibphonenumberPlugin.kt
@@ -10,8 +10,14 @@ import io.flutter.plugin.common.MethodChannel.Result
 
 /** DrLibphonenumberPlugin */
 class DrLibphonenumberPlugin : FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel : MethodChannel
+    
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        // DO NOTHING.
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "dr_libphonenumber")
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
